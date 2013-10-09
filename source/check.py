f2de24563961c6c1b2d72d4ea1815651afd6e9a1
Thomas Schraitle <tom_schr@web.de>
2013-10-09 17:13:03 +0200

    Improve Python code
    - Remove else branch (avoids indentation)
    - Import webbrowser module
    - Use another XML parser

--- source/check.py
+++ source/check.py
@@ -5,6 +5,7 @@
 
 import sys, os, subprocess, shutil
 from lxml import etree
+import webbrowser
 
 if len( sys.argv ) < 2:
   sys.exit( "No file provided.\nUsage: %s FILE" % sys.argv[0] )
@@ -14,16 +15,29 @@ if sys.argv[1] == "--version" or sys.argv[1] == "-v":
   sys.exit( "Style Checker 0.1.0" )
 if not os.path.exists( sys.argv[1] ):
   sys.exit( "File %s provided does not exist.\n" % sys.argv[1] )
-else:
-  inputfile = etree.parse( sys.argv[1] )
-  transform = etree.XSLT( etree.parse( "check.xsl" ) )
-  result = transform( inputfile )
 
-  estring = etree.tostring( result, pretty_print=True )
-  prettyformat = '<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/css" href="checkresult.css" ?>'
-  resultfile = open( '/tmp/checkresult.xml', 'w' )
-  resultfile.write( prettyformat + estring.decode('UTF-8') )
-  resultfile.close()
-  shutil.copyfile( 'check.css', '/tmp/checkresult.css' )
+parser = etree.XMLParser(ns_clean=True,
+                         remove_pis=False,
+                         #resolve_entities=False,
+                         dtd_validation=False)
+inputfile = etree.parse( sys.argv[1] )
+transform = etree.XSLT( etree.parse( "check.xsl", parser ) )
+result = transform( inputfile )
 
-  subprocess.call(['firefox', '/tmp/checkresult.xml'])
+#estring = etree.tounicode( result, pretty_print=True )
+#resultfile = open( '/tmp/checkresult.xml', 'w' )
+#resultfile.write( estring )
+#resultfile.close()
+
+root = result.getroot()
+print("root:", root.getchildren() )
+
+result.write( '/tmp/checkresult.xml', xml_declaration=True, encoding="UTF-8", pretty_print=True)
+
+shutil.copyfile( 'check.css', '/tmp/checkresult.css' )
+
+firefox = webbrowser.get("firefox")
+firefox.open_new_tab(url="/tmp/checkresult.xml")
+# subprocess.call(['firefox', '/tmp/checkresult.xml'])
+
+# EOF
