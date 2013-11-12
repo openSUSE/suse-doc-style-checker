7854d04d4cea724670aef685187a3086826d9697
Thomas Schraitle <toms@suse.de>
2013-11-12 15:11:25 +0100

    Added specs directory

--- setup.py
+++ setup.py
@@ -1,22 +1,49 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+import sys
+import os
+import re
+
 try:
     from distutils.core import setup
 except ImportError, excp:
-    from setuptools import setup
+    print("No distutils module found.", file=sys.stderr)
+    sys.exit(10)
+    #from setuptools import setup
 
-setup(  name = '',
-  # version = pydot.__version__,
+def listdir(path, ignore=None):
+    """Lists all files recursively, starting from a given directory
+    """
+    all=[]
+    for root, sub, files in os.walk(path):
+        for f in files:
+           all.append( os.path.join(root, f) )
+
+    if ignore:
+        all = [ i for i in all for j in ignore if not re.search(j, i) ]
+
+    return all
+
+print("All files: %s" % listdir("specs") )
+
+setup(  name = 'docstylecheck',
+  version = "1.0",
   description = '',
   long_description="",
   author = 'Stefan Knorr',
   author_email = 'sknorr@suse.de',
-  url = '',
-  download_url = '' ,
+  url = 'git@gitorious.org:style-checker/style-checker.git',
+  download_url = 'git@gitorious.org:style-checker/style-checker.git' ,
   license = 'MIT',
-  keywords = 'graphviz dot graphs visualization',
-  classifiers=[],
-  data_files = [('.', ['LICENSE', 'README'])]
+  # keywords = '',
+  # classifiers=[],
+  # install_requires=["lxml"],
+  scripts=["source/docstylecheck.py"],
+  data_files = [('.',     ['LICENSE', 'README.md'] ),
+                ('specs', listdir("specs", (".tmp", ".profiled")) ),
+               ],
+  # package_data={ 'specs': ['specs']},
 )
 
 # EOF
