0a4d2bc89a7ca95686e803ca0c1fdf07743f5ac6
Stefan Knorr <sknorr@suse.de>
2013-11-12 14:33:45 +0100

    Try to make the best of the argparse switch
    * add _programname_ & (useful) __license__
    * set usage by hand, so argparse does not uselessly list all parameters
    * try to fit in with argparse's ugly formatting choice of not allowing
      capitalisation or sentences
    * add a big todo
    
    Color me unconvinced of argparse

--- source/docstylecheck.py
+++ source/docstylecheck.py
@@ -4,40 +4,40 @@
 import sys, os.path, subprocess, webbrowser, glob, re, argparse
 from lxml import etree
 
-programname = "Documentation Style Checker"
+__programname__ = "Documentation Style Checker"
 __version__ = "0.1.0pre"
-__author__  = "Stefan Knorr"
-__license__ = "..."
-__description__ = "Documentation Style Checker"
+__author__ = "Stefan Knorr"
+__license__ = "MIT"
+__description__ = "checks a given DocBook XML file for stylistic errors"
 
 openfile = False
 
 arguments = sys.argv
-location = os.path.dirname( os.path.realpath(__file__) )
+location = os.path.dirname( os.path.realpath( __file__ ) )
 
+# TODO: Get rid of the entire "positional arguments" thing that argparse adds
+# (self-explanatory anyway). Get rid of "optional arguments" header. Make sure
+# least important options (--help, --version) are listed last. Also, I really
+# liked being able to use sentences in the parameter descriptions.
 def parseargs():
-   parser = argparse.ArgumentParser(description=__description__,
-                                    )
-   parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+   parser = argparse.ArgumentParser(
+      usage= __file__ + " [options] inputfile [outputfile]",
+      description=__description__ )
+   parser.add_argument('-v', '--version',
+      action = 'version',
+      version = __programname__ + ' ' + __version__,
+      help = "show version number and exit")
    parser.add_argument( '-s', '--show',
-      action='store_true',
-      default=False,
-      help="Show final report in $BROWSER, or default browser if unset. "
-           "Not all browsers open report files correctly and for some "
-           "users, a text editor will open. In such cases, set the "
-           "BROWSER variable with: export BROWSER=/MY/FAVORITE/BROWSER "
-           "Chromium or Firefox will both do the right thing."
-           )
-   # 
-   parser.add_argument('inputfile',
-            #type=argparse.FileType('r'),
-            )
-   parser.add_argument('outputfile',
-            nargs="?",
-            #default=sys.stdout,
-            #type=argparse.FileType('r'),
-            )
-    
+      action = 'store_true',
+      default = False,
+      help = """show final report in $BROWSER, or default browser if unset; not
+          all browsers open report files correctly and for some users, a text
+          editor will open; in such cases, set the BROWSER variable with:
+          export BROWSER=/MY/FAVORITE/BROWSER ; Chromium or Firefox will both
+          do the right thing""" )
+   parser.add_argument( 'inputfile' )
+   parser.add_argument( 'outputfile', nargs="?" )
+
    return parser.parse_args()
 
 args = parseargs()
