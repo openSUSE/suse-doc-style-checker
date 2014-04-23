5f17c6aa3d2d41eef27dcc26b1781ee88193e425
Stefan Knorr <sknorr@suse.de>
2014-04-23 18:07:40 +0200

    Some rather useless consistency changes

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ __description__ = "checks a given DocBook XML file for stylistic errors"
 try:
     from distutils.core import setup
 except ImportError:
-    print("No distutils module found.", file=sys.stderr)
+    print("No distutils module found.", file = sys.stderr)
     sys.exit(10)
 
 
@@ -34,20 +34,20 @@ def listdir(path, ignore=None):
 
 print("All files: %s" % listdir("specs"))
 
-setup(name='suse-doc-style-checker',
-      version=__version__,
-      description=__description__,
-      long_description="suse-doc-style-checker ",
-      author=__author__,
-      author_email='sknorr@suse.de',
-      url='git@gitorious.org:style-checker/style-checker.git',
-      download_url='git@gitorious.org:style-checker/style-checker.git',
-      license=__license__,
-      data_files=[('.',                  ['LICENSE', ]),
-                  ('source/',            listdir('source/')),
-                  ('bookmarklet/',       listdir('bookmarklet')),
-                  ('man/',               listdir('man')),
-                 ],
+setup(name = 'suse-doc-style-checker',
+      version = __version__,
+      description = __description__,
+      long_description = "suse-doc-style-checker ",
+      author = __author__,
+      author_email = 'sknorr@suse.de',
+      url = 'https://www.gitorious.org/style-checker/style-checker',
+      download_url = 'https://www.gitorious.org/style-checker/style-checker',
+      license = __license__,
+      data_files =  [('.',                  ['LICENSE', ]),
+                    ('source/',            listdir('source')),
+                    ('bookmarklet/',       listdir('bookmarklet')),
+                    ('man/',               listdir('man')),
+                    ],
       )
 
 # EOF
