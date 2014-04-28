0a16beb02621c580f72d3d44ad367238b67aad8b
Stefan Knorr <sknorr@suse.de>
2014-04-28 19:08:31 +0200

    Bump version to RC2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import re
 
 # FIXME: this information should come from the script itself...
 __programname__ = "SUSE Documentation Style Checker"
-__version__ = "2014-02.2.rc1"
+__version__ = "2014-02.2.rc2"
 __author__ = "Stefan Knorr, Thomas Schraitle"
 __license__ = "MIT"
 __description__ = "checks a given DocBook XML file for stylistic errors"
