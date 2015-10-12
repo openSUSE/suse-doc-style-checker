84f64841f90c681d1e3df974bd52281fb438ee4d
Thomas Schraitle <toms@suse.de>
2015-10-12 11:27:48 +0200

    Added Bash script for Python env

---
+++ make_devsetup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# this make file sets up the test environment 
+#
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ENVFOLDER="$SCRIPTDIR/env"
+
+if [ -d $ENVFOLDER ]; then
+	echo "There is already an environment folder in the root folder of the project."
+	exit 1
+fi
+
+cd $SCRIPTDIR
+pyvenv env
+source env/bin/activate
+pip install --upgrade pip setuptools
+pip install -r test_requirements.txt
+python3 setup.py develop
