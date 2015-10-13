6574a1ed579b41003c4ec9b38ff1b1b6c88d8468
Stefan Knorr <sknorr@suse.de>
2015-10-13 12:36:16 +0200

    Correct for attribute name change + add debug message type

--- src/sdsc/cli.py
+++ src/sdsc/cli.py
@@ -83,11 +83,13 @@ def printcolor( message, messagetype = None ):
     """Print a colored message
 
     :param str message: Message
-    :param str messagetype: Type (none => green, 'error' => red)
+    :param str messagetype: Type (none => green, 'error' => red, 'debug' => blue)
     """
     if sys.stdout.isatty():
-        if type == 'error':
+        if messagetype == 'error':
             print( '\033[0;31m' + message + '\033[0m' )
+        elif messagetype == 'debug':
+            print( '\033[0;36m' + message + '\033[0m' )
         else:
             print( '\033[0;32m' + message + '\033[0m' )
     else:
