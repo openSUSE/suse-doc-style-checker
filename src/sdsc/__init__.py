4464570bcc68a3e77e92cd9892a64518903d741e
Stefan Knorr <sknorr@suse.de>
2016-03-17 16:16:36 +0100

    Add exception for project spelling of openSUSE
    & fix test case for it. Also, Wicked should be capitalized, in fact, in
    the openSUSE wiki, it is.

--- src/sdsc/__init__.py
+++ src/sdsc/__init__.py
@@ -51,7 +51,7 @@ apostrophe = re.compile( r'[’՚\'ʼ＇｀̀́`´ʻʹʽ]' )
 # Lookbehinds need to have a fixed length... thus .ca
 # Do not use capture groups together with re.split, as it does additional splits
 # for each capture group it finds.
-sentenceends = re.compile( r'(?<![Ee]\.g|etc|[Ii]\.e|.ca|[Nn]\.[Bb]|[Ii]nc)(?:\.?\.?[.!?]|[:;])\s+[[({]?(?=[A-Z0-9#])' )
+sentenceends = re.compile( r'(?<![Ee]\.g|etc|[Ii]\.e|.ca|[Nn]\.[Bb]|[Ii]nc)(?:\.?\.?[.!?]|[:;])\s+[[({]?(?=(?:[A-Z0-9#]|openSUSE))' )
 lastsentenceends = re.compile( r'(?<![Ee]\.g|etc|[Ii]\.e|.ca|[Nn]\.[Bb]|[Ii]nc)(?:\.?\.?[.!?][])}]?|[:;])\s*$' )
 
 
