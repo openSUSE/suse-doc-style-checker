9cb91a7e7a3b1eb92549fdb475bc04dce406e9e0
Fabian Vogt <fvogt@suse.com>
2016-03-23 15:30:25 +0100

    Fix some warnings reported by pylint

--- tests/test_sdsc.py
+++ tests/test_sdsc.py
@@ -1,69 +1,67 @@
-import pytest
-
 import os
-import sdsc
 import sys
 import re
+import pytest
 from lxml import etree
+import sdsc
 
 
 def test_sdsc_version(capsys):
     """checks for output of sdsc --version"""
     with pytest.raises(SystemExit):
         sdsc.main(["--version"])
-    out, err = capsys.readouterr()
+    out, _ = capsys.readouterr()
     assert sdsc.__version__ == out.split()[-1]
 
 
 def test_tokenizer():
     """checks whether the tokenizer works as expected"""
     tokens = sdsc.tokenizer("This is a simple sentence.")
-    assert(tokens == ["This", "is", "a", "simple", "sentence."])
+    assert tokens == ["This", "is", "a", "simple", "sentence."]
     tokens = sdsc.tokenizer("This is a less simple sentence with a\xa0nbsp.")
-    assert(tokens == ["This", "is", "a", "less",
-                      "simple", "sentence", "with", "a", "nbsp."])
+    assert tokens == ["This", "is", "a", "less",
+                      "simple", "sentence", "with", "a", "nbsp."]
 
 
 def test_sentencesegmenter():
     """checks whether sentencesegmenter behaves sane"""
-    sentences = sdsc.sentencesegmenter(
-        "This is a simple ##@command-2## sentence. This one as well.")
-    assert(sentences == [
-           "This is a simple ##@command-2## sentence", "This one as well"])
+    sentences = sdsc.sentencesegmenter("This is a simple ##@command-2## sentence. This one too.")
+    assert sentences == ["This is a simple ##@command-2## sentence", "This one too"]
     sentences = sdsc.sentencesegmenter(
         "This is not a test in one go. openSUSE is not written with a capital letter.")
-    assert(sentences == ["This is not a test in one go",
-                         "openSUSE is not written with a capital letter"])
+    assert sentences == ["This is not a test in one go",
+                         "openSUSE is not written with a capital letter"]
     sentences = sdsc.sentencesegmenter("This is a sentence, e.g. for me.")
-    assert(sentences == ["This is a sentence, e.g. for me"])
+    assert sentences == ["This is a sentence, e.g. for me"]
     sentences = sdsc.sentencesegmenter("E. g. this is a sentence.")
-    assert(sentences == ["E. g. this is a sentence"])
+    assert sentences == ["E. g. this is a sentence"]
     sentences = sdsc.sentencesegmenter(
         "An above average chance stands e.g. Michael. Marta is also on the list.")
-    assert(sentences == ["An above average chance stands e.g. Michael",
-                         "Marta is also on the list"])
+    assert sentences == ["An above average chance stands e.g. Michael",
+                         "Marta is also on the list"]
 
 
 def test_isDupe():
+    """checks whether isDupe is correct"""
     tokens = ["this", "is", "a", "test"]
-    assert(sdsc.isDupe(tokens, 2) == 0)
+    assert sdsc.isDupe(tokens, 2) == 0
     tokens = ["this", "is", "is", "a", "test"]
-    assert(sdsc.isDupe(tokens, 2) == 1)
+    assert sdsc.isDupe(tokens, 2) == 1
     tokens = ["this", "is", "this", "is", "a", "test"]
-    assert(sdsc.isDupe(tokens, 2) == 2)
+    assert sdsc.isDupe(tokens, 2) == 2
     tokens = ["this", "is", "(this", "is)", "a", "test"]
-    assert(sdsc.isDupe(tokens, 2) == 0)
+    assert sdsc.isDupe(tokens, 2) == 0
 
 
 def test_highlighter():
     """checks whether the highlight function works"""
     xml = sdsc.highlight(["highlight", "these", "two", "words"], 1, 2)
-    assert(xml == "highlight <highlight>these two</highlight> words")
-
-# The xmltestcase fixture returns all files in tests/cases
+    assert xml == "highlight <highlight>these two</highlight> words"
 
 
 def test_xml(xmltestcase):
+    """Runs specified testcase and validates the output of all checks.
+    The xmltestcase fixture returns all files in tests/cases"""
     nr_errors = 0
     try:
         resultxml = sdsc.checkOneFile(xmltestcase)
@@ -75,14 +73,14 @@ def test_xml(xmltestcase):
     inputtree = etree.parse(xmltestcase)
     inputids = []
     for elem in inputtree.getiterator():
-        id = elem.get("id")
-        if id != None:
-            if id.count("sdsc."):
-                if id in inputids:
+        eid = elem.get("id")
+        if eid is not None:
+            if eid.count("sdsc."):
+                if eid in inputids:
                     pytest.fail(
-                        "Duplicate ID {0!r} in case {1!r}!".format(id, xmltestcase))
+                        "Duplicate ID {0!r} in case {1!r}!".format(eid, xmltestcase))
 
-                inputids.append(id)
+                inputids.append(eid)
     if len(inputids) == 0:
         pytest.skip("No tests found in {0}".format(
             os.path.basename(xmltestcase)))
@@ -103,15 +101,15 @@ def test_xml(xmltestcase):
 
             withinid = elem.findtext("location/withinid")
             message = elem.find("message")
-            if withinid == None:
+            if withinid is None:
                 withinid = elem.findtext("message/id")
-                if withinid == None:
+                if withinid is None:
                     pytest.fail("No withinid found")
 
             formattedMessage = "<no message>"
-            if message != None:
+            if message is not None:
                 formattedMessage = etree.tostring(
-                    message, method="text", encoding='UTF-8').decode(encoding='UTF-8')  # bytes to str
+                    message, method="text", encoding='UTF-8').decode(encoding='UTF-8')
                 # Remove excessive whitespace and newlines
                 formattedMessage = " ".join(formattedMessage.split()).strip()
 
@@ -123,25 +121,23 @@ def test_xml(xmltestcase):
         for complaint in complaintList:
             if not complaint["id"].count("sdsc.expect.{0}.{1}".format(complaint["type"], checkmodule)):
                 print("Unexpected {0} {1!r} generated by module {2!r} for ID {3!r}.".format(complaint[
-                      "type"], complaint["message"], checkmodule, complaint["id"]), file=sys.stderr)
+                    "type"], complaint["message"], checkmodule, complaint["id"]), file=sys.stderr)
                 nr_errors += 1
 
     # Now check for missing errors and warnings
-    for id in inputids:
-
-        # id = "sdsc.expect.error.a-an" => idArr = [ "sdsc", "expect", "error",
-        # "a-an" ]
-        findExpect = re.search('.*sdsc\.expect\.([^.]+)\.([^.]+)(?:\..*|$)', id)
+    for eid in inputids:
+        # Regex for test_sdsc special ids ("stuff.sdsc.expect.warning.a-an.1")
+        findExpect = re.search(r'.*sdsc\.expect\.([^.]+)\.([^.]+)(?:\..*|$)', eid)
         if not findExpect:
             continue
         found = False
         if findExpect.group(2) in complaints:
-            found = sum(complaint["id"] == id and complaint["type"] == findExpect.group(
-                        1) for complaint in complaints[findExpect.group(2)])
+            found = sum(complaint["id"] == eid and complaint["type"] == findExpect.group(
+                1) for complaint in complaints[findExpect.group(2)])
 
         if not found:
             print("Expected {0} for ID {1!r} generated by module {2!r}.".format(
-                findExpect.group(1), id, findExpect.group(2)), file=sys.stderr)
+                findExpect.group(1), eid, findExpect.group(2)), file=sys.stderr)
             nr_errors += 1
 
     if nr_errors > 0:
