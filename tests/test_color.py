68d83172c3e0cc374d1852a228ead73e7af5a9e3
Thomas Schraitle <tom_schr@web.de>
2016-09-30 00:21:53 +0200

    Add isatty fixture and use monkeypatch

--- tests/test_color.py
+++ tests/test_color.py
@@ -5,20 +5,22 @@ import re
 from sdsc import printcolor
 
 
-@pytest.mark.parametrize("msg",
-                         ("a", "b\nc")
+@pytest.mark.parametrize("msg",  ("hello",)
 )
 @pytest.mark.parametrize("msgtype",
                          ("error", "debug", None)
 )
-# @pytest.mark.parametrize('isatty', (True, False))
-def test_printcolor(capsys, msg, msgtype):
+@pytest.mark.parametrize('isatty', (True, False))
+def test_printcolor(capsys, monkeypatch, msg, msgtype, isatty):
     """Checks printcolor() function
 
+    :param capsys:
     :param str msg:
     :param str msgtype:
-    :param capsys:
+    :param bool isatty:
     """
+    monkeypatch.setattr('sys.stdout.isatty', lambda: isatty)
+
     printcolor(msg, msgtype)
     if msgtype in ('debug', 'error'):
         assert capsys.readouterr()[0].count(msg) == 0
