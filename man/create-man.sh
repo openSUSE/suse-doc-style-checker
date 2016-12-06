6b76ef1a251734ee2cc8662efe36ef715a0421a0
Stefan Knorr <sknorr@suse.de>
2016-12-06 17:58:32 +0100

    Man page: Use sed for fixing up the path to DB

--- man/create-man.sh
+++ man/create-man.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-DB=$(xmlcatalog /etc/xml/catalog http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl)
-DB=${DB#file:/}
+DB=$(xmlcatalog /etc/xml/catalog http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl | sed -r 's_^file:/+_/_')
 
-xsltproc --output sdsc.1 $DB suse-doc-style-checker.xml
+# The echo really helps debugging when things go wrong and we also don't really
+# want to try using a file called "No" as the stylesheets
+echo "DocBook stylesheet path: $DB"
+if [[ -f ${DB} ]]; then
+  xsltproc --output sdsc.1 $DB suse-doc-style-checker.xml
+else
+  exit 1
+fi
