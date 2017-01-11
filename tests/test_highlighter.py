227d6edd1e7b66285f2c313c0c981e734ae3db43
Stefan Knorr <sknorr@suse.de>
2017-01-11 16:32:56 +0100

    Add more test parameters for the highlighting code

--- tests/test_highlighter.py
+++ tests/test_highlighter.py
@@ -6,13 +6,22 @@ from sdsc import highlight
 
 @pytest.mark.parametrize("tokens,start,end,expected",
  (
-   # 1
+   # 1 - already split
    (["highlight", "these", "two", "words"], 1, 2,
      "highlight <highlight>these two</highlight> words"
     ),
-   # 2
+   # 2 - still needs to be split
    ("highlight these two words", 1, 2,
     "highlight <highlight>these two</highlight> words"),
+   # 3 - longer example
+   (["highlight", "the", "next", "few", "of", "these", "superbly", "chosen", "and", "well-intentioned", "words"], 5, 7,
+    "highlight the next few of <highlight>these superbly chosen</highlight> and well-intentioned words"),
+   # 4 - example where highlight is out-of-bounds (end)
+   ("highlight the next few of these superbly chosen and well-intentioned words", 5, 25,
+    "highlight the next few of <highlight>these superbly chosen and well-intentioned words</highlight>"),
+   # 5 - example where highlight is out-of-bounds (start)
+   ("highlight the first word", -4, 0,
+    "<highlight>highlight</highlight> the first word"),
  )
 )
 def test_highlighter(tokens, start, end, expected):
