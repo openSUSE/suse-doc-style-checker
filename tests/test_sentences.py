1636fe834830ebb6644d17f908f893a3c2a41e33
Stefan Knorr <sknorr@suse.de>
2017-01-11 18:27:51 +0100

    Expand the sentence segmentation tests a little()

--- tests/test_sentences.py
+++ tests/test_sentences.py
@@ -6,26 +6,36 @@ from sdsc import sentencesegmenter
 
 @pytest.mark.parametrize("sentence,expected",
 (
-   # 1
-   ("This is a simple ##@command-2## sentence. This one too.",
-    ["This is a simple ##@command-2## sentence", "This one too"]),
-   # 2
+   # 0 - a single simple sentence
+   ("This is a simple sentence.",
+    ["This is a simple sentence"]),
+   # 1 - two simple sentences
+   ("This is a simple ##@command-2## sentence. This one is too.",
+    ["This is a simple ##@command-2## sentence", "This one is too"]),
+   # 2 - lowercase letter starts second sentence
    ("This is not a test in one go. openSUSE is not written with a capital letter.",
     ["This is not a test in one go",
      "openSUSE is not written with a capital letter"]),
-   # 3
+   # 3 - abbreviation in the middle of the sentence
    ("This is a sentence, e.g. for me.",
     ["This is a sentence, e.g. for me"]),
-   # 4
+   # 4 - abbreviation at the start of the sentence
    ("E. g. this is a sentence.",
     ["E. g. this is a sentence"]),
-   # 5
+   # 5 - abbreviation in the middle of sentence before a capital letter
    ("An above average chance stands e.g. Michael. Marta is also on the list.",
     ["An above average chance stands e.g. Michael",
      "Marta is also on the list"]),
-   # Add more entries here:
+   # 6 - sentences with parentheses around them
+   ("(We speak in circles. We dance in code.)",
+    ["We speak in circles",
+     "We dance in code"]),
+   # 6 - sentences with parentheses around them
+   ("We speak in circles. (We dance in code.)",
+    ["We speak in circles",
+     "We dance in code"]),
 ))
 def test_sentencesegmenter(sentence, expected):
-    """checks whether sentencesegmenter behaves sane"""
+    """checks whether sentencesegmenter behaves sanely"""
     sentences = sentencesegmenter(sentence)
     assert sentences == expected
